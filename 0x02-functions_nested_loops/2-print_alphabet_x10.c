30a1d0c2b6be497de63db7e8ac9536e047cca2b8
Getachew <getachew.mekonen12@gmail.com>
2023-02-21 18:58:15 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - prints 10 times the alphabet, in lowercase, 
+* print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 * followed by a new line
 * Return: void
 */
 
 void print_alphabet_x10(void)
 {
-		
+
 	char ch;
 	int i = 0;
-
 	while (i < 10)
 	{
 	ch = 'a';
