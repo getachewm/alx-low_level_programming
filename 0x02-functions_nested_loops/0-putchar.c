aab311c55fbd5160c9bc9d3cc120eb6b0bced820
Getachew <getachew.mekonen12@gmail.com>
2023-02-21 17:54:13 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
-	/**
-	 * main - Entry point
-	 * Return: Always 0
-	 */
-
+/**
+* main - Entry point
+* Return: Always 0
+*/
 
 	int main(void)
 	{
@@ -18,3 +17,4 @@
 	_putchar('\n');
 	return (0);
 	}
+
